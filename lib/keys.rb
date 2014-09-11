949d2adc3798af15632ac89e1ab64999a49eaad5
Andy Wong <alacritythief@gmail.com>
2014-09-11 13:11:37 -0400

    Minor code polish

--- lib/keys.rb
+++ lib/keys.rb
@@ -17,22 +17,21 @@ module Keys
     end
   end
 
-    def button_up(id)
-      if id == Gosu::KbW
-        @paddle_left.velocity += 8
-      end
-
-      if id == Gosu::KbS
-        @paddle_left.velocity += -8
-      end
-
-      if id == Gosu::KbI
-        @paddle_right.velocity += 8
-      end
-
-      if id == Gosu::KbK
-        @paddle_right.velocity += -8
-      end
-
+  def button_up(id)
+    if id == Gosu::KbW
+      @paddle_left.velocity += 8
     end
+
+    if id == Gosu::KbS
+      @paddle_left.velocity += -8
+    end
+
+    if id == Gosu::KbI
+      @paddle_right.velocity += 8
+    end
+
+    if id == Gosu::KbK
+      @paddle_right.velocity += -8
+    end
+  end
 end
