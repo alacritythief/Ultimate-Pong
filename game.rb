cd9fd334b5073c860a18b5143e143c4336e718b2
Andy Wong <alacritythief@gmail.com>
2014-09-15 14:58:30 -0400

    More refactoring. Moved particles to ball object, made basic "smart AI" that doesn't lose... yet.

--- game.rb
+++ game.rb
@@ -27,16 +27,6 @@ class GameWindow < Gosu::Window
 
     @ai_on = Gosu::Font.new(self, "helvetica", 20)
     @ready_to_serve = Gosu::Font.new(self, "helvetica", 20)
-
-    @fireball = Ashton::ParticleEmitter.new 0, 0, 3,
-                                           scale: 8,
-                                           speed: 1..100,
-                                           max_particles: 1000,
-                                           offset: 0..10,
-                                           interval: 0.0010,
-                                           color: Gosu::Color.rgba(255, 150, 0, 50),
-                                           fade: 70,
-                                           gravity: 0
   end
 
   def update
@@ -45,7 +35,6 @@ class GameWindow < Gosu::Window
     @paddle_right.update
 
     computer_ai
-    particle_calc
 
     if @paddle_left.collide?(@bouncing_ball.left, @bouncing_ball.top) == true
       @bouncing_ball.vx = +5
@@ -62,12 +51,21 @@ class GameWindow < Gosu::Window
     if @bouncing_ball.x < -30
       @bouncing_ball = Ball.new(self, 730, 290)
       @score.player_right += 1
+      reset_paddle_positions
     end
 
     if @bouncing_ball.right > 830
       @bouncing_ball = Ball.new(self, 40, 290)
       @score.player_left += 1
+      reset_paddle_positions
     end
+  end
+
+  def reset_paddle_positions
+    @paddle_left.x = 10
+    @paddle_left.y = 250
+    @paddle_right.x = 770
+    @paddle_right.y = 250
   end
 
   def draw
@@ -75,40 +73,23 @@ class GameWindow < Gosu::Window
     @paddle_left.draw
     @paddle_right.draw
     @score.draw
-    @fireball.draw
     @ai_on.draw("AI #{@ai_status}", 370, 30, 0, 1.0, 1.0, 0xffffffff)
 
     if @bouncing_ball.in_play? == false
       @ready_to_serve.draw("Press SPACE to serve!", 310, 300, 0, 1.0, 1.0, 0xffffffff)
     end
-
   end
 
   def computer_ai
     if @toggle_ai == true
       @ai_status = "ON"
 
-      if @bouncing_ball.y > 580 && @bouncing_ball.x > 0
-        @paddle_right.velocity = [-6,-5,-5].sample
-      end
-
-      if @bouncing_ball.y < 0 && @bouncing_ball.x > 0
-        @paddle_right.velocity = [6,5,5].sample
-      end
+      @paddle_right.velocity = @bouncing_ball.vy
 
     else
       @ai_status = "OFF"
     end
   end
-
-  def particle_calc
-    @last_update_at ||= Gosu::milliseconds
-    particle_timer = [Gosu::milliseconds - @last_update_at, 100].min * 0.001
-    @last_update_at = Gosu::milliseconds
-
-    @fireball.update particle_timer
-    @fireball.x, @fireball.y = @bouncing_ball.x + 10, @bouncing_ball.y + 10
-    end
 end
 
 window = GameWindow.new
--- lib/ball.rb
+++ lib/ball.rb
@@ -1,6 +1,6 @@
 class Ball
   include BoundingBox
-  attr_accessor :x, :y, :vx, :left, :right, :top, :bottom, :serve
+  attr_accessor :x, :y, :vx, :vy, :left, :right, :top, :bottom, :serve, :speed
 
   def initialize(window, x, y)
     @ball_image = Gosu::Image.new(window, "img/ball.png", false)
@@ -9,18 +9,42 @@ class Ball
     @vx = @vy = 0
     @serve = false
     @ball_in_play = false
+    @speed = 0
+
+    @particle = Ashton::ParticleEmitter.new 0, 0, 3,
+                                           scale: 8,
+                                           speed: 1..100,
+                                           max_particles: 1000,
+                                           offset: 0..10,
+                                           interval: 0.0010,
+                                           color: Gosu::Color.rgba(255, 150, 0, 50),
+                                           fade: 70,
+                                           gravity: 0
+
     bounding(@x, @y, 20, 20)
   end
 
   def draw
     @ball_image.draw(@x, @y, 1)
+    @particle.draw
   end
 
   def in_play?
     @ball_in_play
   end
 
+  def particle_calc
+    @last_update_at ||= Gosu::milliseconds
+    particle_timer = [Gosu::milliseconds - @last_update_at, 100].min * 0.001
+    @last_update_at = Gosu::milliseconds
+
+    @particle.update particle_timer
+    @particle.x, @particle.y = @x + 10, @y + 10
+  end
+
   def update
+    particle_calc
+
     if @serve == true
       if x < 400
         @vx = 5
